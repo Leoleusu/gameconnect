0b2cfca388f37430d9252595f143f1181cd3c254
EC2 Default User <ec2-user@ip-172-31-35-65.ap-northeast-3.compute.internal>
2022-11-23 13:43:10 +0000

    user fix[gc]

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,13 @@ Rails.application.routes.draw do
     get 'comments/show'
     get 'comments/edit'
     get 'comments/destroy'
+    post 'guest_sign_in', to: 'sessions#guest_sign_in'
   end
   devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
