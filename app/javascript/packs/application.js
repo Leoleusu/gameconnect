d61a9d43a9a3ebc4908374d1c415f52d756fed3f
EC2 Default User <ec2-user@ip-172-31-47-52.ap-northeast-3.compute.internal>
2023-01-22 09:35:35 +0000

    sidemenu plus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -25,21 +26,3 @@ $(document).ready(function() {
   $('.drawer').drawer();
 });
 })(jQuery);
-
-$('.drawer').drawer({
-  class: {
-    nav: 'drawer-nav',
-    toggle: 'drawer-toggle',
-    overlay: 'drawer-overlay',
-    open: 'drawer-open',
-    close: 'drawer-close',
-    dropdown: 'drawer-dropdown'
-  },
-  iscroll: {
-    // Configuring the iScroll
-    // https://github.com/cubiq/iscroll#configuring-the-iscroll
-    mouseWheel: true,
-    preventDefault: false
-  },
-  showOverlay: true
-});
