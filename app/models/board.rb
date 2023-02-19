42da35f6c3cdf47556b7349c73feaa3873ae6024
EC2 Default User <ec2-user@ip-172-31-47-52.ap-northeast-3.compute.internal>
2023-02-19 13:57:21 +0000

    fix board_comments

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,5 +4,5 @@ class Board < ApplicationRecord
   has_many :board_comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 20 }#「presence: true」で入力必須。「length: { maximum:  }」は文字数制限のバリデーション。今回は、２５５字以内という制限。
-  validates :body, presence: true, length: { maximum: 1_535 }
+  validates :body, presence: true, length: { maximum: 1_500 }
 end
--- app/models/board_comment.rb
+++ app/models/board_comment.rb
@@ -2,5 +2,5 @@ class BoardComment < ApplicationRecord
   belongs_to :user
   belongs_to :board
 
-  validates :body, presence: true, length: { maximum: 1_535 }
+  validates :body, presence: true, length: { maximum: 1_500 }
 end
